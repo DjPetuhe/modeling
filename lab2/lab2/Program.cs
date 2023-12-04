c37a10cdc099d8a22dfc547cb0c8142b6df21808
DjPetuhe <validhernufKPI@gmail.com>
2023-12-04 23:21:42 +0200

    Lab2: add model examples

--- lab2/lab2/Program.cs
+++ lab2/lab2/Program.cs
@@ -1,28 +1,65 @@
-﻿
-using lab2;
+﻿using lab2;
 using lab2.Elements;
 using lab2.Generators;
 
-IGenerator generator1 = new ConstantGenerator(1);
-IGenerator generator2 = new UniformGenerator(3, 4);
-IGenerator generator3 = new UniformGenerator(10, 20);
+namespace lab2
+{
+    public class Programm
+    {
+        public static void Main(string[] args)
+        {
+            //ModelFromPicture();
+            ComplexModel();
+        }
 
-Create cr = new("Create", generator1);
+        public static void ModelFromPicture()
+        {
+            IGenerator generatorCreate = new ExponentialGenerator(5);
+            IGenerator generatorProcess1 = new ExponentialGenerator(5);
+            IGenerator generatorProcess2 = new ExponentialGenerator(5);
+            IGenerator generatorProcess3 = new ExponentialGenerator(5);
 
-ComplexProcess pr1 = new("Process1", generator2, 5, 3);
-ComplexProcess pr2 = new("Process2", generator3, 5, 3);
-ComplexProcess pr3 = new("Process3", generator3, 5, 3);
+            Create cr = new("Create", generatorCreate);
 
-cr.AddNextElement(pr1, 1);
+            Process pr1 = new("Process1", generatorProcess1, 5);
+            Process pr2 = new("Process2", generatorProcess2, 5);
+            Process pr3 = new("Process3", generatorProcess3, 5);
 
-pr1.AddNextElement(pr2, 1);
-pr1.AddNextElement(pr3, 1);
+            cr.AddNextElement(pr1, 1);
 
-//pr2.AddNextElement(pr1, 1);
-pr2.AddNextElement(null, 1);
+            pr1.AddNextElement(pr2, 1);
+            pr2.AddNextElement(pr3, 1);
 
-//pr3.AddNextElement(pr1, 1);
-pr3.AddNextElement(null, 1);
+            Model mod = new(new List<Element>() { cr, pr1, pr2, pr3 });
+            mod.Simulate(1000);
+        }
 
-Model mod = new(new List<Element>() { cr, pr1, pr2, pr3 });
-mod.Simulate(100);
+        public static void ComplexModel()
+        {
+            IGenerator generatorCreate = new ExponentialGenerator(5);
+            IGenerator generatorProcess1 = new ExponentialGenerator(5);
+            IGenerator generatorProcess2 = new ExponentialGenerator(10);
+            IGenerator generatorProcess3 = new ExponentialGenerator(10);
+
+            Create cr = new("Create", generatorCreate);
+
+            ComplexProcess pr1 = new("Process1", generatorProcess1, 5, 5);
+            ComplexProcess pr2 = new("Process2", generatorProcess2, 5, 3);
+            ComplexProcess pr3 = new("Process3", generatorProcess3, 5, 3);
+
+            cr.AddNextElement(pr1, 1);
+
+            pr1.AddNextElement(pr2, 1);
+            pr1.AddNextElement(pr3, 1);
+
+            pr2.AddNextElement(null, 3);
+            pr2.AddNextElement(pr1, 1);
+
+            pr3.AddNextElement(null, 3);
+            pr3.AddNextElement(pr1, 1);
+
+            Model mod = new(new List<Element>() { cr, pr1, pr2, pr3 });
+            mod.Simulate(1000);
+        }
+    }
+}
